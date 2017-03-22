eb86644c0dcc77ae91beae12795bc95e617b763e
Shiang-Yun Yang <morris821028@gmail.com>
2017-03-22 06:30:26 -0700

    Ubuntu version package

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install cgroup-tools -y
+#sudo apt-get install cgroup-tools -y
+sudo apt-get install cgroup-bin cgroup-lite libcgroup1 -y
 sudo apt-get install build-essential -y
