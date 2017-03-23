177ec69afe18510ab35736308c05477e2a62a0d0
Shiang-Yun Yang <morris821028@gmail.com>
2017-03-23 11:13:18 +0800

    comment

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Debian
 #sudo apt-get install cgroup-tools -y
+
+# Ubuntu
 sudo apt-get install cgroup-bin cgroup-lite libcgroup1 -y
+
+# Common
 sudo apt-get install build-essential -y
